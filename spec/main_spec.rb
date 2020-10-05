153bb1865e74fd196683f778cdf5e6b36aed918e
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-10-05 21:26:01 +0200

    fix rubocop errors

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,70 +1,70 @@
 require_relative '../main'
 
 describe 'Enumerables' do
-  let(:arr){[5,6,9,8,4]}
-  let(:hash){{name:"Mariana", color:"blue", location:"mexico" }}
-  let(:range){(0..7)}
+  let(:arr) { [5, 6, 9, 8, 4] }
+  let(:hash) { { name: 'Mariana', color: 'blue', location: 'mexico' } }
+  let(:range) { (0..7) }
 
   describe '#my_each' do
-    it  'give an enum if not block given' do
-      expect(arr.my_each.is_a? Enumerable).to be(true)
-	  end
+    it 'give an enum if not block given' do
+      expect((arr.my_each.is_a? Enumerable)).to be(true)
+    end
 
-	  it 'yield block to given array'  do
-		  arr1 = []
-		  arr.my_each{|n| arr1 << n*2}
-		  expect(arr1).to eq([10, 12, 18, 16, 8])
-	  end
-	
-	  it 'if a hash is given block should  yield' do
-	    color_given = []
-	    hash.my_each{|k, v| color_given << v}
-      expect(color_given).to eq(["Mariana","blue", "mexico"])
-	  end
-	
-    it 'when range is given block yield'do 
-		  range2 = []
-		  range.my_each{|el| range2 << el / 2.0}
-		  expect(range2).to eq([0,0.5,1,1.5,2,2.5,3,3.5])
+    it 'yield block to given array' do
+      arr1 = []
+      arr.my_each { |n| arr1 << n * 2 }
+      expect(arr1).to eq([10, 12, 18, 16, 8])
+    end
+
+    it 'if a hash is given block should yield' do
+      color_given = []
+      hash.my_each { |_k, v| color_given << v }
+      expect(color_given).to eq(%w[Mariana blue mexico])
+    end
+
+    it 'when range is given block yield' do
+      range2 = []
+      range.my_each { |el| range2 << el / 2.0 }
+      expect(range2).to eq([0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5])
     end
 
     it 'Return the elements 1 2 3 4' do
-	    expect(arr.my_each { |x| x }).to eql(arr)
+      expect(arr.my_each { |x| x }).to eql(arr)
     end
   end
 
   describe '#my_each_with_index' do
     it 'Return self' do
-	    expect(arr.my_each_with_index { |x, y| }).to eql(arr)
+      expect(arr.my_each_with_index { |x, y| }).to eql(arr)
     end
 
     it 'Return an enumerator when no block given' do
-	    result = arr.my_each_with_index
-	    expect(result.class).to eq(Enumerator)
+      result = arr.my_each_with_index
+      expect(result.class).to eq(Enumerator)
     end
 
     it 'calls the block on a given array and returns it' do
       val = 0
-      arr.my_each_with_index {|value , i|  val= value if (i==3)}
+      arr.my_each_with_index { |value, i| val = value if i == 3 }
       expect(val).to eq(8)
     end
 
     it 'should call the block on a hash and return it' do
       val = ''
-      hash.my_each_with_index{|value, i| val = value if i == 2}
-      expect(val).to eq([:location, "mexico"])
+      hash.my_each_with_index { |value, i| val = value if i == 2 }
+      expect(val).to eq([:location, 'mexico'])
     end
 
     it 'should call the block on a range and return it' do
       val = 0
-      range.my_each_with_index{|value, i| val = value if i == 5}
+      range.my_each_with_index { |value, i| val = value if i == 5 }
       expect(val).to eq(5)
     end
   end
 
   describe '#my_all?' do
     it 'Should return true if all elements of an array pass a condition given in a block' do
-      expect(arr.my_all?{|num| num > 2 }).to be true
+      expect(arr.my_all? { |num| num > 2 }).to be true
     end
 
     it 'Should return false if all elements of an array do not pass a condition given in a block' do
@@ -80,8 +80,7 @@ describe 'Enumerables' do
     end
 
     it 'Should return true if all elements of a hash pass a condition given in a block' do
-      expect(hash.all?{|key, val| val.is_a? String}).to be true
+      expect(hash.all? { |_key, val| val.is_a? String }).to be true
     end
-    
   end
 end
