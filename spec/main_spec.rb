2e539dda03837d2a659fc5a337f0b34950e1497f
atenaiis <attennaisc@gmail.com>
2020-10-05 10:53:58 -0500

    testing each_with_index

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -21,12 +21,52 @@ describe '#my_each' do
    expect(color_given).to eq(color)
 	end
 	
-  it 'when range is given block yield'do 
-  	range = (0..7)
+    it 'when range is given block yield'do 
+    	range = (0..7)
  		result = [0,0.5,1,1.5,2,2.5,3,3.5]
 		range2 = []
 		range.my_each{|el| range2 << el / 2.0}
 		expect(range2).to eq(result)
   end
 
+   it 'Return the elements 1 2 3 4' do
+	 expect([1, 2, 3, 4].my_each { |x| x }).to eql([1, 2, 3, 4])
+   end
+end
+
+
+describe '#my_each_with_index' do
+    it 'Return self' do
+	    expect(%w[hello world].my_each_with_index { |x, y| }).to eql(%w[hello world])
+   end
+
+   it 'Return an enumerator when no block given' do
+	    result = [1, 2, 6].my_each_with_index
+	     expect(result.class).to eq(Enumerator)
+  end
+
+    it 'return array when given block ' do
+      arr = %w[m a r i a n a]
+      val = ""
+      arr.my_each_with_index {|value , i|  val= value if (i==3)}
+      expect(val).to eq('i')
+    end  
+end
+
+ 
+	
+describe '#my_all?' do
+    it 'return true' do
+      expect(%w[ant bear cat].my_all? { |word| word.length >= 3 }).to eql(true)
+    end
+    it 'return false' do
+      expect(%w[ant bear cat].my_all? { |word| word.length >= 4 }).to eql(false)
+    end
+    it 'return false' do
+      expect([1, 2i, 3.14].my_all?(Numeric)).to eql(true)
+    end
+    it 'return false' do
+      expect([nil, true, 99].my_all?).to eql(false)
+    end
+    
 end
